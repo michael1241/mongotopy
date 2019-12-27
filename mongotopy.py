faf44a9ea2c9dab55dfbde6d00059aa76c6a5ab1
michael1241 <michael@jamcupboard.co.uk>
2019-12-27 21:02:05 +0000

    first commit

---
+++ mongotopy.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+import os
+import subprocess
+import json
+
+seconds = '10'
+filename = "mongodataforlukhas"
+
+def reformat(data):
+    formatted = []
+    data = data['totals']
+    for dbcoll in data:
+        database, coll = dbcoll.split(".",1)
+        for op in ["read", "write"]:
+            for field in ["time", "count"]:
+                formatted.append(f'"database":"{database}", "coll":"{coll}", "op":"{op}", "field":"{field}", "value":{data[dbcoll][op][field]}')
+    return "\n".join(formatted)
+
+def saveMongoData():
+    mongocall = subprocess.Popen(['mongotop', '--json', '--rowcount=1', seconds], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout,stderr = mongocall.communicate()
+    mongodata = reformat(json.loads(stdout.decode("utf-8")))
+    with open('tmpFile', 'w') as f:
+        f.write(mongodata)
+    os.rename('tmpFile', filename)
+
+while True:
+    saveMongoData()
