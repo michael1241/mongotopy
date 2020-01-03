8e12ec0f5a98d5daa1d3f09df616b14fd8f1ca07
michael1241 <michael@jamcupboard.co.uk>
2020-01-03 13:54:01 +0000

    making export array of json objects

--- mongotopy.py
+++ mongotopy.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python3
+
 import os
 import subprocess
 import json
 
-seconds = '10'
+seconds = '1'
 filename = "mongodataforlukhas"
 
 def reformat(data):
@@ -13,15 +14,18 @@ def reformat(data):
         database, coll = dbcoll.split(".",1)
         for op in ["read", "write"]:
             for field in ["time", "count"]:
-                formatted.append('"database":"{}", "coll":"{}", "op":"{}", "field": "{}", "value":{}'.format(database, coll, op, field, data[dbcoll][op][field]))
-    return "\n".join(formatted)
+                formatted.append({"database":database, "coll":coll, "op":op, "field": field, "value":data[dbcoll][op][field]})
+    return formatted
 
 def saveMongoData():
     mongocall = subprocess.Popen(['mongotop', '--json', '--rowcount=1', seconds], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     stdout,stderr = mongocall.communicate()
+
     mongodata = reformat(json.loads(stdout.decode("utf-8")))
+
     with open('tmpFile', 'w') as f:
-        f.write(mongodata)
+        f.write(str(mongodata))
+
     os.rename('tmpFile', filename)
 
 while True:
